8aad47103752c90bd14d03797486b3ee353754ea
Aleksandr Kartavykh <alex-krt@protonmail.com>
2019-10-29 15:57:42 +0300

    ansible-3

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -20,6 +20,19 @@ resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip" 
 }
 
+resource "google_compute_firewall" "firewall_http" {
+  name = "allow-http-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["reddit-app"]
+  
+}
+
+
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
   network = "default"
